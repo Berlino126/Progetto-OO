bb3a3cac7e84f3326a54a725057f122b28867b70
Berlino126 <126323198+Berlino126@users.noreply.github.com>
2023-09-27 16:09:10 +0200

    Update Libro.java

--- src/main/java/com/mycompany/bibiotecadigitale/model/Libro.java
+++ src/main/java/com/mycompany/bibiotecadigitale/model/Libro.java
@@ -1,15 +1,21 @@
+
 package com.mycompany.bibiotecadigitale.model;
 import java.util.Date;
+
 public class Libro extends Testo {
     private String genere;
     private int capitoli;
     private int pagine;
+    private String eventoPresentazione;
+    private String collana;
 
-    public Libro(Integer codTesto, String titolo, Date annoPubblicazione, String edizione, boolean disponibilita, String formato, String tipologia, String genere, int capitoli, int pagine) {
+    public Libro(Integer codTesto, String titolo, Date annoPubblicazione, String edizione, boolean disponibilita, String formato, String tipologia, String genere, int capitoli, int pagine, String eventoPresentazione, String collana) {
         super(codTesto, titolo, annoPubblicazione, edizione, disponibilita, formato, tipologia);
         this.genere = genere;
         this.capitoli = capitoli;
         this.pagine = pagine;
+        this.eventoPresentazione = eventoPresentazione;
+        this.collana = collana;
     }
 
     public String getGenere() {
@@ -34,5 +40,21 @@ public class Libro extends Testo {
 
     public void setPagine(int pagine) {
         this.pagine = pagine;
+    }
+
+    public String getEventoPresentazione() {
+        return eventoPresentazione;
+    }
+
+    public void setEventoPresentazione(String eventoPresentazione) {
+        this.eventoPresentazione = eventoPresentazione;
+    }
+
+    public String getCollana() {
+        return collana;
+    }
+
+    public void setCollana(String collana) {
+        this.collana = collana;
     }
 }
