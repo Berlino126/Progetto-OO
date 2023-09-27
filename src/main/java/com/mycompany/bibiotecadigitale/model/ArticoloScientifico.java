a7597bb1caaf2dd692b05a9ad08fa1f1b9eacdbb
Berlino126 <126323198+Berlino126@users.noreply.github.com>
2023-09-27 15:59:33 +0200

    Update ArticoloScientifico.java

--- src/main/java/com/mycompany/bibiotecadigitale/model/ArticoloScientifico.java
+++ src/main/java/com/mycompany/bibiotecadigitale/model/ArticoloScientifico.java
@@ -1,13 +1,25 @@
 package com.mycompany.bibiotecadigitale.model;
+
 import java.util.Date;
+
 public class ArticoloScientifico extends Testo {
     private String universita;
     private String riassunto;
+    private String nomeRivista;
+    private String argomento;
+    private String responsabile;
+    private String luogoConferenza;
+    private Date dataConferenza;
 
-    public ArticoloScientifico(Integer codTesto, String titolo, Date annoPubblicazione, String edizione, boolean disponibilita, String formato, String tipologia, String universita, String riassunto) {
+    public ArticoloScientifico(Integer codTesto, String titolo, Date annoPubblicazione, String edizione, boolean disponibilita, String formato, String tipologia, String universita, String riassunto, String nomeRivista, String argomento, String responsabile, String luogoConferenza, Date dataConferenza) {
         super(codTesto, titolo, annoPubblicazione, edizione, disponibilita, formato, tipologia);
         this.universita = universita;
         this.riassunto = riassunto;
+        this.nomeRivista = nomeRivista;
+        this.argomento = argomento;
+        this.responsabile = responsabile;
+        this.luogoConferenza = luogoConferenza;
+        this.dataConferenza = dataConferenza;
     }
 
     public String getUniversita() {
@@ -25,4 +37,45 @@ public class ArticoloScientifico extends Testo {
     public void setRiassunto(String riassunto) {
         this.riassunto = riassunto;
     }
+
+    public String getNomeRivista() {
+        return nomeRivista;
+    }
+
+    public void setNomeRivista(String nomeRivista) {
+        this.nomeRivista = nomeRivista;
+    }
+
+    public String getArgomento() {
+        return argomento;
+    }
+
+    public void setArgomento(String argomento) {
+        this.argomento = argomento;
+    }
+
+    public String getResponsabile() {
+        return responsabile;
+    }
+
+    public void setResponsabile(String responsabile) {
+        this.responsabile = responsabile;
+    }
+
+    public String getLuogoConferenza() {
+        return luogoConferenza;
+    }
+
+    public void setLuogoConferenza(String luogoConferenza) {
+        this.luogoConferenza = luogoConferenza;
+    }
+
+    public Date getDataConferenza() {
+        return dataConferenza;
+    }
+
+    public void setDataConferenza(Date dataConferenza) {
+        this.dataConferenza = dataConferenza;
+    }
 }
+
