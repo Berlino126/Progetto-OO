32f222202dc5f0ba6e06f6a7c20794b0ed75b203
Berlino126 <126323198+Berlino126@users.noreply.github.com>
2023-09-27 16:00:11 +0200

    Update ArticoloScientificoDAO.java

--- src/main/java/com/mycompany/bibiotecadigitale/dao/ArticoloScientificoDAO.java
+++ src/main/java/com/mycompany/bibiotecadigitale/dao/ArticoloScientificoDAO.java
@@ -14,10 +14,37 @@ public class ArticoloScientificoDAO extends TestoDAO {
         insertTesto(articolo);
 
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO ArticoloScientifico (CodTesto, Universita, Riassunto) VALUES (?, ?, ?)");
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "INSERT INTO ArticoloScientifico (CodTesto, Universita, Riassunto, NomeRivista, Argomento, Responsabile, LuogoConferenza, DataConferenza) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
             preparedStatement.setInt(1, articolo.getCodTesto());
             preparedStatement.setString(2, articolo.getUniversita());
             preparedStatement.setString(3, articolo.getRiassunto());
+            preparedStatement.setString(4, articolo.getNomeRivista());
+            preparedStatement.setString(5, articolo.getArgomento());
+            preparedStatement.setString(6, articolo.getResponsabile());
+            preparedStatement.setString(7, articolo.getLuogoConferenza());
+            preparedStatement.setDate(8, new java.sql.Date(articolo.getDataConferenza().getTime()));
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Gestisci l'eccezione
+        }
+    }
+
+    public void updateArticoloScientifico(ArticoloScientifico articolo) {
+        updateTesto(articolo);
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "UPDATE ArticoloScientifico SET Universita = ?, Riassunto = ?, NomeRivista = ?, Argomento = ?, Responsabile = ?, LuogoConferenza = ?, DataConferenza = ? WHERE CodTesto = ?");
+            preparedStatement.setString(1, articolo.getUniversita());
+            preparedStatement.setString(2, articolo.getRiassunto());
+            preparedStatement.setString(3, articolo.getNomeRivista());
+            preparedStatement.setString(4, articolo.getArgomento());
+            preparedStatement.setString(5, articolo.getResponsabile());
+            preparedStatement.setString(6, articolo.getLuogoConferenza());
+            preparedStatement.setDate(7, new java.sql.Date(articolo.getDataConferenza().getTime()));
+            preparedStatement.setInt(8, articolo.getCodTesto());
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -34,22 +61,9 @@ public class ArticoloScientificoDAO extends TestoDAO {
             e.printStackTrace();
             // Gestisci l'eccezione
         }
+
         // Chiamata alla funzione deleteTesto per eliminare il record dalla tabella Testo
         deleteTesto(codice);
     }
-    public void updateArticoloScientifico(ArticoloScientifico articolo) {
-        updateTesto(articolo);
 
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(
-                    "UPDATE ArticoloScientifico SET Universita = ?, Riassunto = ? WHERE CodTesto = ?");
-            preparedStatement.setString(1, articolo.getUniversita());
-            preparedStatement.setString(2, articolo.getRiassunto());
-            preparedStatement.setInt(3, articolo.getCodTesto());
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            // Gestisci l'eccezione
-        }
-    }
 }
