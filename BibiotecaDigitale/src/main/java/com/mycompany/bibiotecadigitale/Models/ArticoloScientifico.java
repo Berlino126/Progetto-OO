0bea8ca9d75555606ac623b234b787db79f7d5ab
Berlino126 <126323198+Berlino126@users.noreply.github.com>
2023-09-27 15:58:50 +0200

    Update ArticoloScientifico.java

--- BibiotecaDigitale/src/main/java/com/mycompany/bibiotecadigitale/Models/ArticoloScientifico.java
+++ BibiotecaDigitale/src/main/java/com/mycompany/bibiotecadigitale/Models/ArticoloScientifico.java
@@ -1,20 +1,25 @@
-package com.mycompany.bibiotecadigitale.Models;
+package com.mycompany.bibiotecadigitale.model;
 
-public class ArticoloScientifico {
-    public ArticoloScientifico(String codiceTesto, String universita, String riassunto) {
-        this.codiceTesto = codiceTesto;
+import java.util.Date;
+
+public class ArticoloScientifico extends Testo {
+    private String universita;
+    private String riassunto;
+    private String nomeRivista;
+    private String argomento;
+    private String responsabile;
+    private String luogoConferenza;
+    private Date dataConferenza;
+
+    public ArticoloScientifico(Integer codTesto, String titolo, Date annoPubblicazione, String edizione, boolean disponibilita, String formato, String tipologia, String universita, String riassunto, String nomeRivista, String argomento, String responsabile, String luogoConferenza, Date dataConferenza) {
+        super(codTesto, titolo, annoPubblicazione, edizione, disponibilita, formato, tipologia);
         this.universita = universita;
         this.riassunto = riassunto;
-    }
-
-    private String codiceTesto;
-
-    public String getCodiceTesto() {
-        return codiceTesto;
-    }
-
-    public void setCodiceTesto(String codiceTesto) {
-        this.codiceTesto = codiceTesto;
+        this.nomeRivista = nomeRivista;
+        this.argomento = argomento;
+        this.responsabile = responsabile;
+        this.luogoConferenza = luogoConferenza;
+        this.dataConferenza = dataConferenza;
     }
 
     public String getUniversita() {
@@ -33,7 +38,43 @@ public class ArticoloScientifico {
         this.riassunto = riassunto;
     }
 
-    private String universita;
-    private String riassunto;
-    // Aggiungi costruttore, metodi getter e setter
+    public String getNomeRivista() {
+        return nomeRivista;
+    }
+
+    public void setNomeRivista(String nomeRivista) {
+        this.nomeRivista = nomeRivista;
+    }
+
+    public String getArgomento() {
+        return argomento;
+    }
+
+    public void setArgomento(String argomento) {
+        this.argomento = argomento;
+    }
+
+    public String getResponsabile() {
+        return responsabile;
+    }
+
+    public void setResponsabile(String responsabile) {
+        this.responsabile = responsabile;
+    }
+
+    public String getLuogoConferenza() {
+        return luogoConferenza;
+    }
+
+    public void setLuogoConferenza(String luogoConferenza) {
+        this.luogoConferenza = luogoConferenza;
+    }
+
+    public Date getDataConferenza() {
+        return dataConferenza;
+    }
+
+    public void setDataConferenza(Date dataConferenza) {
+        this.dataConferenza = dataConferenza;
+    }
 }
