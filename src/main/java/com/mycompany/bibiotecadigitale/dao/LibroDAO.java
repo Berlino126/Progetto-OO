f18123f6bdaf0253c5ced08036584522d2607ee2
Berlino126 <126323198+Berlino126@users.noreply.github.com>
2023-09-28 14:23:42 +0200

    Update LibroDAO.java

--- src/main/java/com/mycompany/bibiotecadigitale/dao/LibroDAO.java
+++ src/main/java/com/mycompany/bibiotecadigitale/dao/LibroDAO.java
@@ -14,9 +14,9 @@ public class LibroDAO extends TestoDAO {
     public LibroDAO() {
         super();
         // Inizializza la connessione al database qui
-        String url = "jdbc:postgresql://localhost/Bible";
+        String url = "jdbc:postgresql://localhost/DatabaseOO";
         String user = "postgres";
-        String password = "francy09";
+        String password = "profbarra";
         try {
             connection = DriverManager.getConnection(url, user, password);
         } catch (SQLException e) {
@@ -29,7 +29,9 @@ public class LibroDAO extends TestoDAO {
         List<Libro> libri = new ArrayList<>();
         try {
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM Libro");
+            ResultSet resultSet = statement.executeQuery("SELECT L.*, T.Titolo, T.AnnoPubblicazione, T.Edizione, T.Disponibilita, T.Formato, T.Tipologia " +
+                    "FROM Libro L " +
+                    "JOIN Testo T ON L.CodTesto = T.CodTesto");
             while (resultSet.next()) {
                 Integer codice = resultSet.getInt("codTesto");
                 String genere = resultSet.getString("Genere");
@@ -38,14 +40,12 @@ public class LibroDAO extends TestoDAO {
                 String evento = resultSet.getString("Evento");
                 String collana = resultSet.getString("Collana");
                 String titolo = resultSet.getString("Titolo");
-                // Ottieni la data dal database come java.sql.Date
                 java.sql.Date sqlDate = resultSet.getDate("AnnoPubblicazione");
                 Date annoPubblicazione = new Date(sqlDate.getTime());
                 String edizione = resultSet.getString("Edizione");
                 boolean disponibilita = resultSet.getBoolean("Disponibilita");
                 String formato = resultSet.getString("Formato");
                 String tipologia = resultSet.getString("Tipologia");
-
 
                 Libro libro = new Libro(codice, titolo, annoPubblicazione, edizione, disponibilita, formato, tipologia, genere, capitoli, pagine, evento, collana);
                 libri.add(libro);
@@ -57,16 +57,17 @@ public class LibroDAO extends TestoDAO {
         return libri;
     }
 
+
     public void insertLibro(Libro libro) {
         insertTesto(libro);
 
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO Libro (CodTesto, Genere, Capitoli, Pagine, EventoPresentazione, Collana) VALUES (?, ?, ?, ?, ?, ?)");
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO Libro (CodTesto, Genere, Capitoli, Pagine, Evento, Collana) VALUES (?, ?, ?, ?, ?, ?)");
             preparedStatement.setInt(1, libro.getCodTesto());
             preparedStatement.setString(2, libro.getGenere());
             preparedStatement.setInt(3, libro.getCapitoli());
             preparedStatement.setInt(4, libro.getPagine());
-            preparedStatement.setString(5, libro.getEventoPresentazione());
+            preparedStatement.setString(5, libro.getEvento());
             preparedStatement.setString(6, libro.getCollana());
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
@@ -94,7 +95,7 @@ public class LibroDAO extends TestoDAO {
     public void updateLibro(int codice, String genere, int capitoli, int pagine, String evento, String collana) {
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(
-                    "UPDATE Libro SET Genere = ?, Capitoli = ?, Pagine = ?, EventoPresentazione = ?, Collana = ? WHERE CodTesto = ?");
+                    "UPDATE Libro SET Genere = ?, Capitoli = ?, Pagine = ?, Evento = ?, Collana = ? WHERE CodTesto = ?");
             preparedStatement.setString(1, genere);
             preparedStatement.setInt(2, capitoli);
             preparedStatement.setInt(3, pagine);
