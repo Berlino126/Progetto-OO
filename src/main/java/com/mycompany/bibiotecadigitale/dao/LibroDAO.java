44ec1da88798f7bbc5f245b2e64cbd9b25ebadc7
hollowkekko <128052897+hollowkekko@users.noreply.github.com>
2023-09-28 13:04:49 +0200

    Update LibroDAO.java

--- src/main/java/com/mycompany/bibiotecadigitale/dao/LibroDAO.java
+++ src/main/java/com/mycompany/bibiotecadigitale/dao/LibroDAO.java
@@ -1,13 +1,60 @@
 package com.mycompany.bibiotecadigitale.dao;
 
 import com.mycompany.bibiotecadigitale.model.Libro;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import com.mycompany.bibiotecadigitale.model.Testo;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class LibroDAO extends TestoDAO {
+    private static Connection connection;
 
     public LibroDAO() {
         super();
+        // Inizializza la connessione al database qui
+        String url = "jdbc:postgresql://localhost/Bible";
+        String user = "postgres";
+        String password = "francy09";
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Gestisci l'eccezione
+        }
+    }
+
+    public static List<Libro> getAllLibri() {
+        List<Libro> libri = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Libro");
+            while (resultSet.next()) {
+                Integer codice = resultSet.getInt("codTesto");
+                String genere = resultSet.getString("Genere");
+                Integer pagine = resultSet.getInt("Pagine");
+                Integer capitoli = resultSet.getInt("Capitoli");
+                String evento = resultSet.getString("Evento");
+                String collana = resultSet.getString("Collana");
+                String titolo = resultSet.getString("Titolo");
+                // Ottieni la data dal database come java.sql.Date
+                java.sql.Date sqlDate = resultSet.getDate("AnnoPubblicazione");
+                Date annoPubblicazione = new Date(sqlDate.getTime());
+                String edizione = resultSet.getString("Edizione");
+                boolean disponibilita = resultSet.getBoolean("Disponibilita");
+                String formato = resultSet.getString("Formato");
+                String tipologia = resultSet.getString("Tipologia");
+
+
+                Libro libro = new Libro(codice, titolo, annoPubblicazione, edizione, disponibilita, formato, tipologia, genere, capitoli, pagine, evento, collana);
+                libri.add(libro);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Gestisci l'eccezione
+        }
+        return libri;
     }
 
     public void insertLibro(Libro libro) {
@@ -44,18 +91,16 @@ public class LibroDAO extends TestoDAO {
         deleteTesto(codice);
     }
 
-    public void updateLibro(Libro libro) {
-        updateTesto(libro);
-
+    public void updateLibro(int codice, String genere, int capitoli, int pagine, String evento, String collana) {
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(
                     "UPDATE Libro SET Genere = ?, Capitoli = ?, Pagine = ?, EventoPresentazione = ?, Collana = ? WHERE CodTesto = ?");
-            preparedStatement.setString(1, libro.getGenere());
-            preparedStatement.setInt(2, libro.getCapitoli());
-            preparedStatement.setInt(3, libro.getPagine());
-            preparedStatement.setString(4, libro.getEventoPresentazione());
-            preparedStatement.setString(5, libro.getCollana());
-            preparedStatement.setInt(6, libro.getCodTesto());
+            preparedStatement.setString(1, genere);
+            preparedStatement.setInt(2, capitoli);
+            preparedStatement.setInt(3, pagine);
+            preparedStatement.setString(4, evento);
+            preparedStatement.setString(5, collana);
+            preparedStatement.setInt(6, codice);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
