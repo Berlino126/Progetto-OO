cff3d59c70401f643217f097aa42ebfbd1630620
Berlino126 <126323198+Berlino126@users.noreply.github.com>
2023-09-29 15:35:21 +0200

    Update InterfacciaLibro.java

--- src/main/java/com/mycompany/bibiotecadigitale/gui/InterfacciaLibro.java
+++ src/main/java/com/mycompany/bibiotecadigitale/gui/InterfacciaLibro.java
@@ -23,7 +23,8 @@ public class InterfacciaLibro extends javax.swing.JFrame {
     private LibroDAO libroDAO;
     public InterfacciaLibro() {
         initComponents();
-        LibroDAO libroDAO = new LibroDAO();
+        libroDAO = new LibroDAO();
+        refreshLibroTable();
     }
 
     /**
@@ -37,15 +38,15 @@ public class InterfacciaLibro extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
-        ChiudiFinestra = new javax.swing.JLabel();
-        LibriLB = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
         GenereLB = new javax.swing.JLabel();
         GenereTF = new javax.swing.JTextField();
         CapitoliTF = new javax.swing.JTextField();
         CapitoliLB = new javax.swing.JLabel();
         ModificaTesto = new javax.swing.JButton();
         PulisciTesto = new javax.swing.JButton();
-        ScrollTabella = new javax.swing.JScrollPane();
+        jScrollPane1 = new javax.swing.JScrollPane();
         TabellaTesti = new javax.swing.JTable();
         PagineTF = new javax.swing.JTextField();
         PagineLB = new javax.swing.JLabel();
@@ -58,7 +59,6 @@ public class InterfacciaLibro extends javax.swing.JFrame {
         ArticoliLB = new javax.swing.JLabel();
         TestiLB = new javax.swing.JLabel();
         LOGOUTLB = new javax.swing.JLabel();
-        UtentiLB = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(204, 0, 51));
@@ -68,20 +68,20 @@ public class InterfacciaLibro extends javax.swing.JFrame {
 
         jPanel2.setBackground(new java.awt.Color(255, 255, 255));
 
-        ChiudiFinestra.setBackground(new java.awt.Color(204, 0, 51));
-        ChiudiFinestra.setFont(new java.awt.Font("Century Gothic", 1, 24)); // NOI18N
-        ChiudiFinestra.setForeground(new java.awt.Color(204, 0, 51));
-        ChiudiFinestra.setText("x");
-        ChiudiFinestra.addMouseListener(new java.awt.event.MouseAdapter() {
+        jLabel9.setBackground(new java.awt.Color(204, 0, 51));
+        jLabel9.setFont(new java.awt.Font("Century Gothic", 1, 24)); // NOI18N
+        jLabel9.setForeground(new java.awt.Color(204, 0, 51));
+        jLabel9.setText("x");
+        jLabel9.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                ChiudiFinestraMouseClicked(evt);
+                jLabel9MouseClicked(evt);
             }
         });
 
-        LibriLB.setBackground(new java.awt.Color(204, 0, 51));
-        LibriLB.setFont(new java.awt.Font("Century Gothic", 1, 24)); // NOI18N
-        LibriLB.setForeground(new java.awt.Color(204, 0, 51));
-        LibriLB.setText("LIBRI");
+        jLabel3.setBackground(new java.awt.Color(204, 0, 51));
+        jLabel3.setFont(new java.awt.Font("Century Gothic", 1, 24)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(204, 0, 51));
+        jLabel3.setText("LIBRI");
 
         GenereLB.setBackground(new java.awt.Color(204, 0, 51));
         GenereLB.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
@@ -107,12 +107,19 @@ public class InterfacciaLibro extends javax.swing.JFrame {
             }
         });
 
+        TabellaTesti.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                TabellaTestoMouseClicked(evt);
+            }
+        });
+
         PulisciTesto.setText("Pulisci");
         PulisciTesto.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 PulisciTestoMouseClicked(evt);
             }
         });
+
 
         TabellaTesti.setModel(new javax.swing.table.DefaultTableModel(
                 new Object [][] {
@@ -126,14 +133,14 @@ public class InterfacciaLibro extends javax.swing.JFrame {
                 }
         ) {
             boolean[] canEdit = new boolean [] {
-                    true, false, false, false, false, false
+                    false, false, false, false, false
             };
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
                 return canEdit [columnIndex];
             }
         });
-        ScrollTabella.setViewportView(TabellaTesti);
+        jScrollPane1.setViewportView(TabellaTesti);
 
         PagineLB.setBackground(new java.awt.Color(204, 0, 51));
         PagineLB.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
@@ -182,9 +189,9 @@ public class InterfacciaLibro extends javax.swing.JFrame {
                 jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(jPanel2Layout.createSequentialGroup()
                                 .addGap(0, 0, Short.MAX_VALUE)
-                                .addComponent(LibriLB)
+                                .addComponent(jLabel3)
                                 .addGap(269, 269, 269)
-                                .addComponent(ChiudiFinestra)
+                                .addComponent(jLabel9)
                                 .addContainerGap())
                         .addGroup(jPanel2Layout.createSequentialGroup()
                                 .addGap(16, 16, 16)
@@ -228,7 +235,7 @@ public class InterfacciaLibro extends javax.swing.JFrame {
                                                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
                                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(ScrollTabella, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(94, 94, 94))
         );
         jPanel2Layout.setVerticalGroup(
@@ -236,8 +243,8 @@ public class InterfacciaLibro extends javax.swing.JFrame {
                         .addGroup(jPanel2Layout.createSequentialGroup()
                                 .addContainerGap()
                                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                        .addComponent(LibriLB)
-                                        .addComponent(ChiudiFinestra))
+                                        .addComponent(jLabel3)
+                                        .addComponent(jLabel9))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 29, Short.MAX_VALUE)
                                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                                         .addComponent(CapitoliLB)
@@ -264,30 +271,33 @@ public class InterfacciaLibro extends javax.swing.JFrame {
                                         .addComponent(PulisciTesto)
                                         .addComponent(ModificaTesto))
                                 .addGap(18, 18, 18)
-                                .addComponent(ScrollTabella, javax.swing.GroupLayout.PREFERRED_SIZE, 339, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 339, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addContainerGap(25, Short.MAX_VALUE))
         );
 
+
         ArticoliLB.setBackground(new java.awt.Color(204, 0, 51));
         ArticoliLB.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
-        ArticoliLB.setText("ARTICOLI SCIENTIFICI");
+        ArticoliLB.setText("Articoli Scientifici");
         ArticoliLB.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 ArticoloMouseClicked(evt);
             }
         });
 
+
         TestiLB.setBackground(new java.awt.Color(204, 0, 51));
         TestiLB.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
-        TestiLB.setText("TESTI");
+        TestiLB.setText("Testi");
         TestiLB.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 TestoMouseClicked(evt);
             }
         });
 
+
         LOGOUTLB.setBackground(new java.awt.Color(204, 0, 51));
-        LOGOUTLB.setFont(new java.awt.Font("Century Gothic", 1, 24)); // NOI18N
+        LOGOUTLB.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
         LOGOUTLB.setText("LOGOUT");
         LOGOUTLB.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -295,53 +305,37 @@ public class InterfacciaLibro extends javax.swing.JFrame {
             }
         });
 
-        UtentiLB.setBackground(new java.awt.Color(204, 0, 51));
-        UtentiLB.setFont(new java.awt.Font("Century Gothic", 1, 14)); // NOI18N
-        UtentiLB.setText("UTENTI");
-        UtentiLB.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                UtenteMouseClicked(evt);
-            }
-        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
                 jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(14, 14, 14)
                                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                                .addContainerGap()
-                                                .addComponent(ArticoliLB))
-                                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                                .addContainerGap()
-                                                .addComponent(TestiLB))
-                                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                                .addContainerGap()
-                                                .addComponent(UtentiLB))
-                                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                                .addGap(36, 36, 36)
-                                                .addComponent(LOGOUTLB)))
-                                .addGap(20, 20, 20)
+                                        .addComponent(ArticoliLB)
+                                        .addComponent(TestiLB)
+                                        .addComponent(LOGOUTLB))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addContainerGap())
         );
+
         jPanel1Layout.setVerticalGroup(
                 jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addContainerGap()
-                                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(92, 92, 92)
-                                .addComponent(ArticoliLB)
-                                .addGap(18, 18, 18)
-                                .addComponent(TestiLB)
-                                .addGap(18, 18, 18)
-                                .addComponent(UtentiLB)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(LOGOUTLB)
-                                .addGap(51, 51, 51))
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                                .addContainerGap()
+                                                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addGroup(jPanel1Layout.createSequentialGroup()
+                                                .addGap(92, 92, 92)
+                                                .addComponent(ArticoliLB)
+                                                .addGap(18, 18, 18)
+                                                .addComponent(TestiLB)
+                                                .addGap(18, 18, 18)
+                                                .addComponent(LOGOUTLB)))
+                                .addContainerGap(10, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -386,12 +380,10 @@ public class InterfacciaLibro extends javax.swing.JFrame {
                 int pagine = Integer.parseInt(PagineTF.getText());
                 String evento = EventoTF.getText();
                 String collana = CollanaTF.getText();
-
                 libroDAO.updateLibro(codice, genere, capitoli, pagine, evento, collana);
-
                 JOptionPane.showMessageDialog(this, "Testo modificato correttamente");
                 refreshLibroTable();
-                clearTextFields();
+                //clearTextFields();
             } catch (NumberFormatException e) {
                 JOptionPane.showMessageDialog(this, "Il codice testo deve essere un numero intero");
             }
@@ -399,7 +391,7 @@ public class InterfacciaLibro extends javax.swing.JFrame {
     }
 
 
-    private void ChiudiFinestraMouseClicked(java.awt.event.MouseEvent evt) {
+    private void jLabel9MouseClicked(java.awt.event.MouseEvent evt) {
         System.exit(0);
     }
 
@@ -411,8 +403,6 @@ public class InterfacciaLibro extends javax.swing.JFrame {
 
     private void ArticoloMouseClicked(java.awt.event.MouseEvent evt) {
         dispose();
-        InterfacciaArticolo interfacciaArticolo = new InterfacciaArticolo();
-        interfacciaArticolo.setVisible(true);
         //interfacciaarticolo
     }
 
@@ -420,12 +410,6 @@ public class InterfacciaLibro extends javax.swing.JFrame {
         dispose();
         Login login = new Login();
         login.setVisible(true);
-    }
-
-    private void UtenteMouseClicked (java.awt.event.MouseEvent evt) {
-        dispose();
-        ManagerUtenti managerUtenti = new ManagerUtenti();
-        managerUtenti.setVisible(true);
     }
     private void PulisciTestoMouseClicked (java.awt.event.MouseEvent evt)
     {
@@ -457,14 +441,33 @@ public class InterfacciaLibro extends javax.swing.JFrame {
             });
         }
     }
+    private void TabellaTestoMouseClicked(java.awt.event.MouseEvent evt) {
+        DefaultTableModel model = (DefaultTableModel) TabellaTesti.getModel();
+        int Indice = TabellaTesti.getSelectedRow();
+
+        if (Indice != -1) { // Verifica se è stato selezionato un elemento valido
+            CodiceTF.setText(model.getValueAt(Indice, 0) != null ? model.getValueAt(Indice, 0).toString() : "");
+            GenereTF.setText(model.getValueAt(Indice, 1) != null ? model.getValueAt(Indice, 1).toString() : "");
+
+            // Verifica se la data è nulla prima di convertirla in una stringa
+            Object capitoliValue = model.getValueAt(Indice, 2);
+            if (capitoliValue != null) {
+                int capitoli = Integer.parseInt(capitoliValue.toString());
+                CapitoliTF.setText(String.valueOf(capitoli));
+            } else {
+                CapitoliTF.setText("");
+            }
+
+            // Imposta il valore selezionato nei JTextField
+            PagineTF.setText(model.getValueAt(Indice, 3) != null ? model.getValueAt(Indice, 3).toString() : "");
+            EventoTF.setText(model.getValueAt(Indice, 4) != null ? model.getValueAt(Indice, 4).toString() : "");
+            CollanaTF.setText(model.getValueAt(Indice, 5) != null ? model.getValueAt(Indice, 5).toString() : "");
+        }
+    }
+
 
 
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-         */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -510,11 +513,10 @@ public class InterfacciaLibro extends javax.swing.JFrame {
     private javax.swing.JTable TabellaTesti;
     private javax.swing.JLabel TestiLB;
     private javax.swing.JTextField GenereTF;
-    private javax.swing.JLabel UtentiLB;
-    private javax.swing.JLabel LibriLB;
-    private javax.swing.JLabel ChiudiFinestra;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-    private javax.swing.JScrollPane ScrollTabella;
+    private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration
 }
