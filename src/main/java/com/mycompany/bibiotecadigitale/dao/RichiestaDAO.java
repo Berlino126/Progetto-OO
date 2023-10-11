6950f29eeeb071981616c80f327be622abab21c0
Berlino126 <126323198+Berlino126@users.noreply.github.com>
2023-10-11 17:06:48 +0200

    Update RichiestaDAO.java

--- src/main/java/com/mycompany/bibiotecadigitale/dao/RichiestaDAO.java
+++ src/main/java/com/mycompany/bibiotecadigitale/dao/RichiestaDAO.java
@@ -17,18 +17,20 @@ public class RichiestaDAO {
         connection = ConnectionManager.getConnection();
     }
 
-    public static List<Richiesta> getAllRichieste() {
+    public List<Richiesta> getAllRichieste(int codUtente) {
         List<Richiesta> richieste = new ArrayList<>();
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(
-                    "SELECT CodRichiesta, DataRichiesta, CodiceUtente, CodiceTesto, Stato FROM Richiesta");
+                    "SELECT CodRichiesta, DataRichiesta, CodiceUtente, CodiceTesto, Stato FROM Richiesta " +
+                            "WHERE CodiceUtente = ?");
+            preparedStatement.setInt(1, codUtente);
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 Integer codRichiesta = resultSet.getInt("CodRichiesta");
                 Date dataRichiesta = resultSet.getDate("DataRichiesta");
                 Integer codiceUtente = resultSet.getInt("CodiceUtente");
                 Integer codiceTesto = resultSet.getInt("CodiceTesto");
-                String stato = resultSet.getString("Stato"); // Aggiunto stato
+                String stato = resultSet.getString("Stato");
 
                 Richiesta richiesta = new Richiesta(codRichiesta, dataRichiesta, codiceUtente, codiceTesto, stato);
                 richieste.add(richiesta);
@@ -36,23 +38,54 @@ public class RichiestaDAO {
         } catch (SQLException e) {
             e.printStackTrace();
             // Gestisci l'eccezione
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                // Gestisci l'eccezione
+            }
         }
         return richieste;
     }
 
-    public void insertRichiesta(Richiesta richiesta) {
+    public void insertRichiesta(int codUtente, int codTesto) {
+        Connection connection = ConnectionManager.getConnection();
         try {
+            // Ottieni il valore massimo di CodRichiesta dalla tabella
+            PreparedStatement maxIdStatement = connection.prepareStatement(
+                    "SELECT MAX(CodRichiesta) AS MaxCodRichiesta FROM Richiesta");
+            ResultSet maxIdResult = maxIdStatement.executeQuery();
+            int codRichiesta = 1; // Valore di default se non ci sono record nella tabella
+            if (maxIdResult.next()) {
+                codRichiesta = maxIdResult.getInt("MaxCodRichiesta") + 1;
+            }
+
+            // Imposta la data della richiesta come la data corrente
+            Date dataRichiesta = new Date();
+
+            // Imposta lo stato di default a "Valido"
+            String stato = "Valido";
+
             PreparedStatement preparedStatement = connection.prepareStatement(
-                    "INSERT INTO Richiesta (CodRichiesta, DataRichiesta, CodiceUtente, CodiceTesto, Stato) VALUES (?, ?, ?, ?, ?)");
-            preparedStatement.setInt(1, richiesta.getCodRichiesta());
-            preparedStatement.setDate(2, new java.sql.Date(richiesta.getDataRichiesta().getTime()));
-            preparedStatement.setInt(3, richiesta.getCodiceUtente());
-            preparedStatement.setInt(4, richiesta.getCodiceTesto());
-            preparedStatement.setString(5, richiesta.getStato());
+                    "INSERT INTO Richiesta (CodRichiesta, DataRichiesta, CodiceUtente, CodiceTesto, Stato) " +
+                            "VALUES (?, ?, ?, ?, ?)");
+            preparedStatement.setInt(1, codRichiesta);
+            preparedStatement.setTimestamp(2, new java.sql.Timestamp(dataRichiesta.getTime()));
+            preparedStatement.setInt(3, codUtente);
+            preparedStatement.setInt(4, codTesto);
+            preparedStatement.setString(5, stato);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
             // Gestisci l'eccezione
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                // Gestisci l'eccezione
+            }
         }
     }
     public void close() {
@@ -78,4 +111,5 @@ public class RichiestaDAO {
     }
 
 }
+
 
